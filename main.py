ea788824fe29e22661c00dd67de7207abe23e875
shakarjon2024 <rozibayevashakarjon1707@gmail.com>
2025-12-02 15:19:24 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,68 @@
+# 1 - misol
+kopaytirish = lambda x: x * 5
+print(kopaytirish(7))
+
+
+# 2 - misol
+kop = lambda a, b: a * b
+value = kop(3, 7)
+print(value)
+
+
+# 3 - misol
+res = lambda x:  'juft' if x % 2 == 0 else 'toq'
+print(res(7))
+
+
+
+# 4 - misol
+compare = lambda a, b: "a > b" if a > b else ("a < b" if a < b else "a = b")
+print(compare(7, 8))
+print(compare(2, 9))
+
+
+# 5 - misol
+ismlar = ('Bob', 'Alice', 'Lobar', 'Jack')
+ism_uzunligi = list(filter(lambda x: len(x) > 7, ismlar))
+print(ism_uzunligi)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
